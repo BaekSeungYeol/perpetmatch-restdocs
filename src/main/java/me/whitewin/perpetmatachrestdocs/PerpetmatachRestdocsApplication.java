0848de4a18978a32ff236e4487d9620a0e6e1831
BaekSeungYeol <beck33333@naver.com>
2020-08-03 14:12:05 +0900

    first commimt

---
+++ src/main/java/me/whitewin/perpetmatachrestdocs/PerpetmatachRestdocsApplication.java
@@ -0,0 +1,13 @@
+package me.whitewin.perpetmatachrestdocs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerpetmatachRestdocsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PerpetmatachRestdocsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/whitewin/perpetmatachrestdocs/PerpetmatachRestdocsApplicationTests.java
@@ -0,0 +1,13 @@
+package me.whitewin.perpetmatachrestdocs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerpetmatachRestdocsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
